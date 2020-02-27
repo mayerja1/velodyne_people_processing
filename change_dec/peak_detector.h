5703dfe5bb073565da335df1b39d2f0895da5e3b
Jan Mayer <jan-mayer@outlook.com>
2020-02-27 19:54:15 +0100

    rename

--- change_dec/peak_detector.h
+++
@@ -1,9 +0,0 @@
-#ifndef PEAK_DETECTOR_H
-#define PEAK_DETECTOR_H
-#include <vector>
-double mean(std::vector<double>& v);
-
-double stdDev(std::vector<double>& v, double m);
-
-std::vector<int> smoothedZScore(std::vector<double>& input);
-#endif /* end of include guard: PEAK_DETECTOR_H */
